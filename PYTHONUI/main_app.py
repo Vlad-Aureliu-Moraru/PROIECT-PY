7003df22a7980c5f302eea531d6c701896cb1fc7
thorn <moraru.vlad34@gmail.com>
2025-06-08 03:23:24 +0300

    working animation

--- PYTHONUI/main_app.py
+++ PYTHONUI/main_app.py
@@ -1,43 +1,12 @@
 # main_app.py
+
 import sys
 from PyQt5 import QtWidgets
 
-# Import your MainWindow class
-from main_window import MainWindow
+from main_window import MainWindow # Still imports MainWindow
 
-#funct
-
-
-#import_CSV
-
-#import_TXT
-
-#manual_input
-
-
-#calculeaza_func
-
-
-#compare_func
-
-#salveaza_graph
-
-
-
-#salveaza_anim
-
-#set_output
-
-#set_Error
-
-#play_anim
-
-#back_anim
-
-#front_anim
-   
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow() 
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
